2c0546549899205e5e9ccbf88dcfec35cc441b34
CEDROFINANCES\renato.arantes <renato.m.arantes@gmail.com>
2018-02-01 09:43:20 -0200

    commit

---
+++ TestProjetasAPI/TestProjetas/Controllers/VehiclesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestProjetas.Domain.Entities;
+using TestProjetas.Repository;
+
+namespace TestProjetas.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Vehicles")]
+    public class VehiclesController : Controller
+    {
+        // GET: api/Vehicles
+        [HttpGet]
+        public IEnumerable<Vehicle> Get()
+        {
+            return new VehicleRepository().Repository.Get();
+        }
+
+        // GET: api/Vehicles/5
+        [HttpGet("{id}", Name = "Get")]
+        public Vehicle Get(int id)
+        {
+            return new VehicleRepository().Repository.Get(x => x.Id == id).First();
+        }
+
+        // POST: api/Vehicles
+        [HttpPost]
+        public void Post([FromBody]Vehicle value)
+        {
+            new VehicleRepository().Repository.Add(value);
+        }
+
+        // PUT: api/Vehicles
+        [HttpPut]
+        public void Put([FromBody]Vehicle value)
+        {
+            new VehicleRepository().Repository.Update(value);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            new VehicleRepository().Repository.Delete(new Vehicle { Id = id });
+        }
+    }
+}
---
+++ TestProjetasWeb/README.md
@@ -0,0 +1,10 @@
+# TestProjetasAPI
+> Projeto Web utilizando ASP.NET MVC Core 2.0.
+
+## Configuração para Desenvolvimento
+
+É necessário restaurar pacotes nugets e ter versão .net core 2.0.
+
+
+## Meta
+Renato Arantes – renato.m.arantes@gmail.com
