3f6e8950f296711919fe36172a27f22161f9c488
adrianogba <ampontes93@gmail.com>
2018-02-16 00:07:57 -0300

    CRUD de Veiculos com App Android usando WebService feito em PHP conectado a banco de dados MySQL

---
+++ TesteProjetasWebService/dbConnect.php
@@ -0,0 +1,19 @@
+<?php
+	/*
+		author: Belal Khan 
+		website: https://www.simplifiedcoding.net
+		
+		My Database is androiddb 
+		you need to change the database name rest the things are default if you are using wamp or xampp server
+		You may need to change the host user name or password if you have changed the defaults in your server
+	*/
+	
+	//Defining Constants
+	define('HOST','localhost');
+	define('USER','root');
+	define('PASS','');
+	define('DB','testeprojetas');
+	
+	//Connecting to Database
+	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Problemas ao conectar com o banco de dados.');
+	?>
