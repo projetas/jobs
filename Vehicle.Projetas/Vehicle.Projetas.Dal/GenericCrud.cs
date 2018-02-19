95a9c236aa62b6eced884e5b56907492583bc29b
Rogério Franco <34195420+rogeriorfranco@users.noreply.github.com>
2018-02-19 14:11:03 -0300

    Add files via upload

---
+++ Vehicle.Projetas/Vehicle.Projetas.Dal/GenericCrud.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle.Projetas.Dal
+{
+    public abstract class GenericCrud<T> : IGenericCrud<T> where T : class
+    {
+        Context ctx = new Context();
+
+        public void create(T oEntity)
+        {
+            ctx.Set<T>().Add(oEntity);
+            ctx.SaveChanges();
+        }
+
+        public void delete(Func<T, bool> predicate)
+        {
+            ctx.Set<T>()
+           .Where(predicate).ToList()
+           .ForEach(del => ctx.Set<T>().Remove(del));
+            ctx.SaveChanges();
+        }
+
+        public void update(T oEntity)
+        {         
+            ctx.Entry(oEntity).State = EntityState.Modified;
+            ctx.SaveChanges();
+        }
+
+        public IEnumerable<T> readAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public IEnumerable<T> read(Func<T, bool> predicate)
+        {
+            return ctx.Set<T>().Where(predicate);
+        }
+
+    }
+}
---
+++ Vehicle.Projetas/Vehicle.Projetas.Dal/IGenericCrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle.Projetas.Dal
+{
+    public interface IGenericCrud<T> where T : class
+    {
+        void create(T oEntity);
+        void delete(Func<T, bool> predicate);
+        void update(T oEntity);
+        IEnumerable<T> readAll();
+        IEnumerable<T> read(Func<T, bool> predicate);
+    }
+}
