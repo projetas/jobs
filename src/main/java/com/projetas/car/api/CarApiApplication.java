c7e565324c205febe2bf2be49933dec65ebcd557
Vander <vanderzago@gmail.com>
2018-03-18 18:09:58 -0300

    Implementação do CORS para teste do frontend

--- src/main/java/com/projetas/car/api/CarApiApplication.java
+++ src/main/java/com/projetas/car/api/CarApiApplication.java
@@ -2,8 +2,12 @@ package com.projetas.car.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.projetas.car.api.config.CarApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CarApiProperty.class)
 public class CarApiApplication {
 
 	public static void main(String[] args) {
