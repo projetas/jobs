f90f9d74d1ed66e509fb2f86044c1eb517500b3e
mnlel <mnlel@192.168.1.11>
2018-04-09 23:15:50 -0300

    Starting project.

---
+++ vehicle-parent/vehicle-controller/src/test/java/br/projetas/vehicle/VehicleApplicationTests.java
@@ -0,0 +1,17 @@
+package br.projetas.vehicle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VehicleApplicationTests
+{
+
+	@Test
+	public void contextLoads()
+	{}
+
+}
